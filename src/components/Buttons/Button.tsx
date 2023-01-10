56035a40311f5a66882cdfc9951e2fb6e4121cc4
deepshikha77 <deepshikha.ts@neoito.com>
2023-01-10 12:29:47 +0530

    feat: feat

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,25 +2,22 @@ import {Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {C} from '../../constants/index';
 import {t} from 'react-native-tailwindcss';
-const Button = (prop : any) => {
-    const {style,onPress} = prop
-    const handlePress = () => {
-      onPress()
-    }
+const Button = (prop: any) => {
+  const {style, onPress} = prop;
+  const handlePress = () => {
+    onPress();
+  };
   return (
     <TouchableOpacity
-      style={[style,t.itemsCenter,t.justifyCenter,
-        {backgroundColor: C.colors.primaryBlue},C.measures.button
+      style={[
+        style,
+        t.itemsCenter,
+        t.justifyCenter,
+        {backgroundColor: C.colors.primaryBlue},
+        C.measures.button,
       ]}
       onPress={handlePress}>
-      <Text
-        style={[
-          t.fontSemibold,
-          t.textSm,
-          t.textWhite,
-        ]}>
-        Log in
-      </Text>
+      <Text style={[t.fontSemibold, t.textSm, t.textWhite]}>Log in.</Text>
     </TouchableOpacity>
   );
 };
